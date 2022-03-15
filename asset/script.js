72872c5ddf15e9eb44ed14ba03135ba3fc4461ec
Nicolas <nicolaslibert.pro@gmail.com>
2022-03-15 11:44:59 +0100

    Réalisation de l'exercice d'AJAX

---
+++ asset/script.js
@@ -0,0 +1,39 @@
+let poulet = document.getElementById('quote');
+let img = document.getElementById('img');
+let vache = document.getElementById('author');
+
+let prout = new XMLHttpRequest;
+
+prout.onload = function()
+{
+    if (this.status === 200)
+    {
+        console.log(JSON.parse(this.responseText));
+
+        let canard = JSON.parse(prout.responseText)
+        let quote = canard.quote;
+        let author = canard.author.toUpperCase();
+        let photo = canard.photo;
+
+        console.log(quote);
+        console.log(author);
+
+        poulet.textContent = `"${quote}"`;
+        
+        // if(photo == null)
+        // {
+        //     img.src = "ugly_doggy.png";
+        // }
+        // else
+        // {
+        // }
+            img.src = photo;
+        
+        
+        vache.textContent = `- ${author}`;
+    }
+}
+
+prout.open('GET', 'https://thatsthespir.it/api', true)
+
+prout.send();
